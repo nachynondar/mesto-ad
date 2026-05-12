51e7429242ddf2708eebba6adcb03e262d0b71f9
nachyn ondar <ondar.nb@phystech.edu>
2026-05-11 18:05:30 -0700

    fix: add base path for correct styles

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Это критически важно для GitHub Pages!
+  // Теперь пути будут начинаться с /mesto-production/
+  base: '/mesto-production/', 
   server: {
     open: true,
   },
-};
+});
